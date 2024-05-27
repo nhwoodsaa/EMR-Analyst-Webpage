16c9c758ebe93f9342578f43e2b54d9082f844c3
nhwoodsaa <62969869+nhwoodsaa@users.noreply.github.com>
2024-05-27 21:13:59 +1000

    async load the navbar

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
-const navbarContainer = document.getElementById('navbar-container');
-if (navbarContainer) {
-    fetch('https://nhwoodsaa.github.io/navbar.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.text();
-        })
-        .then(data => {
-            navbarContainer.innerHTML = data;
-        })
-        .catch(error => console.error('Error loading navbar:', error));
-} else {
-    console.error('Navbar container element not found');
-}
+(async () => {
+    try {
+        const navbarContainer = document.getElementById('navbar-container');
+        if (!navbarContainer) {
+            console.error('Navbar container element not found');
+            return;
+        }
+
+        const response = await fetch('https://nhwoodsaa.github.io/navbar.html');
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+
+        const data = await response.text();
+        navbarContainer.innerHTML = data;
+    } catch (error) {
+        console.error('Error loading navbar:', error);
+    }
+})();
