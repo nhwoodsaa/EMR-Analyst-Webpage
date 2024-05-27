9c3244f4a1f539794bd9b21dd7fe05e20632556f
nhwoodsaa <62969869+nhwoodsaa@users.noreply.github.com>
2024-05-27 18:08:42 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('navbar.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.text();
-        })
-        .then(data => {
-            const navbarContainer = document.getElementById('navbar-container');
-            if (navbarContainer) {
+    const navbarContainer = document.getElementById('navbar-container');
+    if (navbarContainer) {
+        fetch('navbar.html')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.text();
+            })
+            .then(data => {
                 navbarContainer.innerHTML = data;
-            } else {
-                console.error('Navbar container element not found');
-            }
-        })
-        .catch(error => console.error('Error loading navbar:', error));
+            })
+            .catch(error => console.error('Error loading navbar:', error));
+    } else {
+        console.error('Navbar container element not found');
+    }
 });
